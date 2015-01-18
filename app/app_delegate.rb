91accf9747b73cefe7962047e35aa0c916ac0731
Watson <watson1978@gmail.com>
2015-01-18 14:51:32 +0900

    show help in "docs.esa.io" using default browser

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -6,4 +6,9 @@ class AppDelegate
     request = NSURLRequest.requestWithURL("https://esa.io/".to_nsurl)
     webView.mainFrame.loadRequest(request)
   end
+
+  # actions
+  def showHelp(sender)
+    NSWorkspace.sharedWorkspace.openURL("https://docs.esa.io/".to_nsurl)
+  end
 end
