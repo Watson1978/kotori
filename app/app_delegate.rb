bd63afd2af48e4306121c2dcc3df0ce05a2b18e1
Watson <watson1978@gmail.com>
2015-01-18 15:20:30 +0900

    reopen the window when click app icon in dock

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,10 +1,17 @@
 class AppDelegate
   extend IB
+  outlet :window, NSWindow
   outlet :webView, WebView
 
   def awakeFromNib
+    NSApp.delegate = self    
     request = NSURLRequest.requestWithURL("https://esa.io/".to_nsurl)
     webView.mainFrame.loadRequest(request)
+  end
+
+  def applicationShouldHandleReopen(application, hasVisibleWindows:flag)
+    window.makeKeyAndOrderFront(nil)
+    true
   end
 
   # actions
