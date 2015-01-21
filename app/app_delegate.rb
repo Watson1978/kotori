9c4ec3063afa46fc55c5cfdb4d673dfa49faca20
Watson <watson1978@gmail.com>
2015-01-21 23:44:32 +0900

    add shortcut to "TEAM"

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -56,6 +56,12 @@ class AppDelegate
     loadURL("https://#{team}.esa.io/posts")
   end
 
+  def showTeam(sender)
+    team = teamName()
+    return unless team
+    loadURL("https://#{team}.esa.io/team")
+  end
+
   def showHelp(sender)
     NSWorkspace.sharedWorkspace.openURL("https://docs.esa.io/".to_nsurl)
   end
