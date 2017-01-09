01a28a82634e235da8d9353ecb79e2db2a994d30
Watson <watson1978@gmail.com>
2017-01-09 22:21:38 +0900

    fix that it cannot open a page which has `target="_blank"`

--- kotori/Document.swift
+++ kotori/Document.swift
@@ -1,7 +1,7 @@
 import Cocoa
 import WebKit
 
-class Document: NSDocument, WebFrameLoadDelegate, WebUIDelegate {
+class Document: NSDocument, WebFrameLoadDelegate, WebUIDelegate, WebPolicyDelegate {
     
     var windowController : NSWindowController!
 
@@ -42,16 +42,34 @@ class Document: NSDocument, WebFrameLoadDelegate, WebUIDelegate {
         return windowController.contentViewController?.view as! WebView
     }
 
-    private func loadWebView(_ webView : WebView, url : String) {
-        let request = URLRequest(url: URL(string: url)!)
+    private func loadWebView(_ webView : WebView, request : URLRequest) {
         webView.frameLoadDelegate = self
         webView.uiDelegate = self
+        webView.policyDelegate = self
         webView.mainFrame.load(request)
+    }
+
+    private func loadWebView(_ webView : WebView, url : String) {
+        let request = URLRequest(url: URL(string: url)!)
+        loadWebView(webView, request: request)
     }
     
     // MARK: Delegate - Called when the page title of a frame loads or changes.
     func webView(_ sender: WebView!, didReceiveTitle title: String!, for frame: WebFrame!) {
         windowController?.window?.title = title
+    }
+
+    func webView(_ webView: WebView!, decidePolicyForNewWindowAction actionInformation: [AnyHashable : Any]!, request: URLRequest!, newFrameName frameName: String!, decisionListener listener: WebPolicyDecisionListener!) {
+        var doc : Document
+        do {
+            doc = try (NSDocumentController.shared().openUntitledDocumentAndDisplay(false) as? Document)!
+        }
+        catch {
+            return
+        }
+        doc.makeWindowControllers()
+        loadWebView(getWebView(in: doc.windowController), request: request)
+        doc.showWindows()        
     }
 
     func webView(_ sender: WebView!, runJavaScriptAlertPanelWithMessage message: String!, initiatedBy frame: WebFrame!) {
