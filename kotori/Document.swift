075df4707f0fbfb5f02426ea683fbe6f8495da2c
Watson <watson1978@gmail.com>
2017-01-10 11:56:49 +0900

    fix in where open link which has `target="_blank"'

--- kotori/Document.swift
+++ kotori/Document.swift
@@ -46,21 +46,23 @@ class Document: NSDocument, WebFrameLoadDelegate, WebUIDelegate, WebPolicyDelega
         return windowController.contentViewController?.view as! WebView
     }
 
-    private func loadWebView(_ webView: WebView, request: URLRequest) {
-        webView.frameLoadDelegate = self
-        webView.uiDelegate = self
-        webView.policyDelegate = self
+    private func loadWebView(_ webView: WebView, request: URLRequest, delegate: Document) {
+        webView.frameLoadDelegate = delegate
+        webView.uiDelegate = delegate
+        webView.policyDelegate = delegate
         webView.mainFrame.load(request)
     }
 
     private func loadWebView(_ webView: WebView, url: String) {
         let request = URLRequest(url: URL(string: url)!)
-        loadWebView(webView, request: request)
+        loadWebView(webView, request: request, delegate: self)
     }
     
     // MARK: Delegate - Called when the page title of a frame loads or changes.
     func webView(_ sender: WebView!, didReceiveTitle title: String!, for frame: WebFrame!) {
-        windowController?.window?.title = title
+        if (windowController?.window?.title.hasPrefix("Untitle"))! {
+            windowController?.window?.title = title
+        }
     }
 
     func webView(_ webView: WebView!, decidePolicyForNewWindowAction actionInformation: [AnyHashable : Any]!, request: URLRequest!, newFrameName frameName: String!, decisionListener listener: WebPolicyDecisionListener!) {
@@ -72,7 +74,7 @@ class Document: NSDocument, WebFrameLoadDelegate, WebUIDelegate, WebPolicyDelega
             return
         }
         doc.makeWindowControllers()
-        loadWebView(getWebView(in: doc.windowController), request: request)
+        loadWebView(getWebView(in: doc.windowController), request: request, delegate: doc)
         doc.showWindows()        
     }
 
