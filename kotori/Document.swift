5e333ad61847b1283eae6d968c620c3142d7bd3f
Watson <watson1978@gmail.com>
2017-01-16 08:44:53 +0900

    handle WebView in where ViewController

--- kotori/Document.swift
+++ kotori/Document.swift
@@ -2,8 +2,6 @@ import Cocoa
 import WebKit
 
 class Document: NSDocument, WKNavigationDelegate, WKUIDelegate {
-    
-    var windowController : NSWindowController!
 
     override init() {
         super.init()
@@ -17,33 +15,23 @@ class Document: NSDocument, WKNavigationDelegate, WKUIDelegate {
     override var isDocumentEdited: Bool {
         get { return false }
     }
-    
-    private func makeWindowControllers(withURLString url: String) {
+
+    func makeWindowControllersOnly() {
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
-        windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
-        windowController.window!.contentView = WKWebView()
-        loadWebView(getWebView(in: windowController), url: url)
+        let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
         self.addWindowController(windowController)
     }
 
-    private func makeWindowControllers(withURLString url: String, withConfiguration configuration: WKWebViewConfiguration) {
-        let storyboard = NSStoryboard(name: "Main", bundle: nil)
-        windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
-        windowController.window!.contentView = WKWebView(frame: windowController.window!.frame as CGRect!, configuration: configuration)
-        loadWebView(getWebView(in: windowController), url: url)
-        self.addWindowController(windowController)
+    func makeWindowControllers(withURLString url: String) {
+        makeWindowControllersOnly()
+        let viewController : ViewController = self.windowControllers.first!.contentViewController as! ViewController
+        viewController.load(withURLString: url)
     }
 
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
         let url = getStartPage()
         makeWindowControllers(withURLString: url)
-    }
-
-    func makeWindowControllers(withConfiguration configuration: WKWebViewConfiguration) {
-        // Returns the Storyboard that contains your Document window.
-        let url = getStartPage()
-        makeWindowControllers(withURLString: url, withConfiguration: configuration)
     }
 
     func makeWindowControllers(withPageName page: String) {
@@ -59,57 +47,6 @@ class Document: NSDocument, WKNavigationDelegate, WKUIDelegate {
             url += "/"
         }
         return url
-    }
-    
-    private func getWebView(in windowController: NSWindowController) -> WKWebView {
-        return windowController.window!.contentView as! WKWebView
-    }
-
-    private func loadWebView(_ webView: WKWebView!, request: URLRequest!, delegate: Document) {
-        webView.navigationDelegate = delegate
-        webView.uiDelegate = delegate
-        webView.load(request)
-    }
-
-    private func loadWebView(_ webView: WKWebView, url: String) {
-        let request = URLRequest(url: URL(string: url)!)
-        loadWebView(webView, request: request, delegate: self)
-    }
-    
-    // MARK: Delegate - Called when the page title of a frame loads or changes.
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        windowController.window!.title = webView.title ?? ""
-    }
-
-    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-        let doc = try! NSDocumentController.shared().openUntitledDocumentAndDisplay(false) as! Document
-
-        doc.makeWindowControllers(withConfiguration: configuration)
-        let webView = getWebView(in: doc.windowController)
-        loadWebView(webView, request: navigationAction.request, delegate: doc)
-        doc.showWindows()
-        return webView
-    }
-    
-    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
-        let alert = NSAlert()
-        alert.messageText = message
-        alert.addButton(withTitle: "OK")
-        alert.alertStyle = NSAlertStyle.warning
-        alert.runModal()
-        completionHandler()
-    }
-
-    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
-        let alert = NSAlert()
-        alert.messageText = message
-        alert.addButton(withTitle: "OK")
-        alert.addButton(withTitle: "Cancel")
-        alert.alertStyle = NSAlertStyle.warning
-        if alert.runModal() == NSAlertFirstButtonReturn {
-            return completionHandler(true)
-        }
-        return completionHandler(false)
     }
 }
 
--- kotori/ViewController.swift
+++ kotori/ViewController.swift
@@ -1,9 +1,62 @@
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKNavigationDelegate, WKUIDelegate {
+    var webView : WKWebView!
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        webView = WKWebView()
+        webView.autoresizingMask =  NSAutoresizingMaskOptions(arrayLiteral: .viewWidthSizable, .viewHeightSizable)
+        self.view = webView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    func load(withRequest request: URLRequest) {
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.load(request)
+    }
+    
+    func load(withURLString url: String) {
+        let request = URLRequest(url: URL(string: url)!)
+        load(withRequest: request)
+    }
+
+    // MARK: Delegate - Called when the page title of a frame loads or changes.
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.view.window!.title = webView.title ?? ""
+    }
+
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        NSWorkspace.shared().open(navigationAction.request.url!)
+        return nil
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alert = NSAlert()
+        alert.messageText = message
+        alert.addButton(withTitle: "OK")
+        alert.alertStyle = NSAlertStyle.warning
+        alert.runModal()
+        completionHandler()
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alert = NSAlert()
+        alert.messageText = message
+        alert.addButton(withTitle: "OK")
+        alert.addButton(withTitle: "Cancel")
+        alert.alertStyle = NSAlertStyle.warning
+        if alert.runModal() == NSAlertFirstButtonReturn {
+            return completionHandler(true)
+        }
+        return completionHandler(false)
     }
 }
 
