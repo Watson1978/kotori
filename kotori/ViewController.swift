54c910e40fa4b1c14fd0cc7a62fa177adc0e46b0
Watson <watson1978@gmail.com>
2017-06-10 11:29:55 +0900

    fix build error on macOS 10.13 + Xcode 9 beta

--- kotori/ViewController.swift
+++ kotori/ViewController.swift
@@ -64,8 +64,8 @@ class ViewController: NSViewController, WKNavigationDelegate, WKUIDelegate {
     // MARK: Delegate - Called when the page title of a frame loads or changes.
     func webView(_: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         if let event = NSApp.currentEvent {
-            let commandKey = Int(event.modifierFlags.rawValue & NSCommandKeyMask.rawValue) != 0
-            let mouseUp = event.type == NSLeftMouseUp
+            let commandKey = Int(event.modifierFlags.rawValue & NSEventModifierFlags.command.rawValue) != 0
+            let mouseUp = event.type == .leftMouseUp
             if commandKey && mouseUp {
                 // Open new window or tab with command key + click
                 do {
